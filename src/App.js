0c44d41a3955fd46c7a59b1286b379d2ceb7329e
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2023-08-26 17:50:03 +0200

    fetch objects ids from api + fetch random object data and display title and image in artwork component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import { useState, useEffect } from 'react';
+
 import logo from './logo_boby_museum.png';
 import './App.css';
 import Artwork from './components/Artwork';
 
 function App() {
+  const departementId = 11
+  const baseURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${departementId}`
+  const [artworkIds, setArtworkIds] = useState([]);
+
+  useEffect(() => {
+    // Effectuer la première requête pour récupérer la liste d'IDs
+    fetch(baseURL)
+      .then(response => response.json())
+      .then(data => {
+        console.trace()
+        setArtworkIds(data.objectIDs); // Mettre à jour l'état avec la liste d'IDs
+      })
+      .catch(error => {
+        console.error('Erreur lors de la requête API pour les IDs :', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Boby Museum</h1>
         <img src={logo} className="App-logo" alt="logo" width="200" height="200" />
       </header>
-      <Artwork />
+      <Artwork artworkIds={artworkIds}/>
     </div>
   );
 }
--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,16 +1,40 @@
+import { useState, useEffect } from 'react';
 import logo from '../logo_boby_museum.png';
 import './Artwork.css';
 
-function Artwork() {
+function Artwork({ artworkIds }) {
+  const [artworkData, setArtworkData] = useState(null);
+  const baseUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
 
+  useEffect(() => {
+    if (artworkIds.length > 0) {
+      // Choisis un ID au hasard de la liste d'IDs
+      const randomId = artworkIds[Math.floor(Math.random() * artworkIds.length)];
+
+      // Effectuer la deuxième requête pour récupérer les informations de l'œuvre
+      fetch(`${baseUrl}${randomId}`)
+        .then(response => response.json())
+        .then(data => {
+          setArtworkData(data); // Mettre à jour l'état avec les données de l'API
+        })
+        .catch(error => {
+          console.error('Erreur lors de la requête API pour les détails de l\'œuvre :', error);
+        });
+    }
+  }, [artworkIds]);
   return (
     <div className="artwork">
-      <h1>Lorem ipsum</h1>
-      <img src={logo} className="App-logo" alt="logo" width="400" height="400" />
-      <div className="artwork-form">
-        <input type="text" />
-        <button type="button">Go</button>
-      </div>
+       {artworkData && (
+        <div className="artwork">
+          <h2>{artworkData.title}</h2>
+          <img src={artworkData.primaryImageSmall} alt={artworkData.title} width="400" height="400" />
+          <div className="artwork-form">
+            <input type="text" />
+            <button type="button">Go</button>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 }
