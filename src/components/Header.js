0b2ca9220195675e51e20f1b91138e93e50a8ed6
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2023-08-28 09:25:59 +0200

    change size logo + artwork picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="App-header">
       <h1>Boby Museum</h1>
-      <img src={logo} className="App-logo" alt="logo" width="200" height="200" />
+      <img src={logo} className="App-logo" alt="logo" width="150" height="150" />
     </header>
   )
 }
